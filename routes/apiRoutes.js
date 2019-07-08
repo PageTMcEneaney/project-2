ebed5aa1a483d03d3488674d4dbb2db311c9cf72
PTMcEneaney <pagemceneaney@gmail.com>
2019-07-07 22:48:56 -0700

    fix syntax error in apiRoutes, update models/example.js to better reflect song data and remove timestamps

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,12 +3,18 @@ var Spotify = require("node-spotify-api");
 var keys = require("./../keys.js");
 var spotify = new Spotify(keys.spotify);
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Get all examples
-  app.get("/api/favorites", function(req, res) {
-    db.Songs.findAll({}).then(function(dbKaraoke) {
-      res.json(dbKaraoke);
-      console.table(dbKaraoke);
+  app.get("/api/songs", function (req, res) {
+    db.Songs.findAll({}).then(function (data) {
+      res.json(data);
+    });
+  });
+
+  app.get("/api/songs/artist/:query", function(req, res) {
+    var query = req.params.query;
+    db.Songs.findAll({ artist: query }).then(function(data) {
+      res.json(data);
     });
   });
 
@@ -21,31 +27,31 @@ module.exports = function(app) {
         return console.log("Error occurred: " + err);
       }
 
-      var condensed =
-        "\nArtist: " +
-        data.tracks.items[0].album.artists[0].name +
-        "\nTrack: " +
-        data.tracks.items[0].name +
-        "\nGenre: " +
-        data.tracks.items[0].name +
-        "\nAlbum: " +
-        data.tracks.items[0].album.name +
-        "\nLink: " +
-        data.tracks.items[0].href +
-        "\n------------------------------";
-      console.log("You searched: " + );
-      console.log(condensed);
+      // var condensed =
+      //   "\nArtist: " +
+      //   data.tracks.items[0].album.artists[0].name +
+      //   "\nTrack: " +
+      //   data.tracks.items[0].name +
+      //   "\nGenre: " +
+      //   data.tracks.items[0].name +
+      //   "\nAlbum: " +
+      //   data.tracks.items[0].album.name +
+      //   "\nLink: " +
+      //   data.tracks.items[0].href +
+      //   "\n------------------------------";
+      // console.log(condensed);
+    });
   });
 
   // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Songs.create(req.body).then(function(dbExample) {
+  app.post("/api/examples", function (req, res) {
+    db.Songs.create(req.body).then(function (dbExample) {
       res.json(dbExample);
     });
   });
 
   // Delete an example by id
-  app.delete("/api/track/:id", function(req, res) {
+  app.delete("/api/track/:id", function (req, res) {
     db.Example.destroy({ where: { id: req.params.id } }).then(function (dbExample) {
       res.json(dbExample);
     });
