7c31a35b07c06980233b4f6a330c4aa8241cbdd4
PTMcEneaney <pagemceneaney@gmail.com>
2019-07-09 10:11:30 -0700

    update /api with switch statement

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,19 +3,52 @@ var Spotify = require("node-spotify-api");
 var keys = require("./../keys.js");
 var spotify = new Spotify(keys.spotify);
 
-module.exports = function (app) {
+module.exports = function(app) {
   // Get all examples
-  app.get("/api/songs", function (req, res) {
-    db.Songs.findAll({}).then(function (data) {
+  app.get("/api/songs", function(req, res) {
+    db.Songs.findAll({}).then(function(data) {
       res.json(data);
     });
   });
 
-  app.get("/api/songs/artist/:query", function(req, res) {
-    var query = req.params.query;
-    db.Songs.findAll({ artist: query }).then(function(data) {
-      res.json(data);
-    });
+  app.get("/api/:type/:query", function(req, res) {
+    var type = req.params.type;
+    var query = req.params.query.split("+").join(" ");
+
+    console.log("query: " + query)
+
+    switch (type) {
+    case "title":
+      db.Songs.findAll({ where: { title: query } }).then(function(data) {
+        res.json(data);
+      });
+      break;
+    case "artist":
+      db.Songs.findAll({ where: { artist: query } }).then(function(data) {
+        res.json(data);
+      });
+      break;
+    case "year":
+      db.Songs.findAll({ where: { year: query } }).then(function(data) {
+        res.json(data);
+      });
+      break;
+    case "genre":
+      db.Songs.findAll({ where: { genre: query } }).then(function(data) {
+        res.json(data);
+      });
+      break;
+    case "album":
+      db.Songs.findAll({ where: { album: query } }).then(function(data) {
+        res.json(data);
+      });
+      break;
+    case "duet":
+      db.Songs.findAll({ where: { duet: query } }).then(function(data) {
+        res.json(data);
+      });
+      break;
+    };
   });
 
   app.post("/api/spotify/:type/:query", function(req, res) {
@@ -44,15 +77,17 @@ module.exports = function (app) {
   });
 
   // Create a new example
-  app.post("/api/examples", function (req, res) {
-    db.Songs.create(req.body).then(function (dbExample) {
+  app.post("/api/examples", function(req, res) {
+    db.Songs.create(req.body).then(function(dbExample) {
       res.json(dbExample);
     });
   });
 
   // Delete an example by id
-  app.delete("/api/track/:id", function (req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function (dbExample) {
+  app.delete("/api/track/:id", function(req, res) {
+    db.Example.destroy({ where: { id: req.params.id } }).then(function(
+      dbExample
+    ) {
       res.json(dbExample);
     });
   });
